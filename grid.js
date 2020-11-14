24ae0060fe8131909c409fa2f159c0ef9e40076b
Liangchen Zhou <387820460@qq.com>
2020-11-14 23:05:43 +0800

    Create grid.js

---
+++ grid.js
@@ -0,0 +1,30 @@
+
+function isSameIsland(grid, row1, col1, row2, col2) {
+    const nearby = isNearby({x: row1, y: col1}, {x: row2, y: col2});
+    if (!nearby) {
+        return false;
+    }
+
+    if (grid[row1][col1] === 1 && grid[row2][col2] === 1) {
+        return true;
+    }
+}
+
+function isNearby(a, b, grid = [[]]) {
+    if (a.x < 0 || a.x > grid.length || a.y < 0 || a.y > grid[0].length) {
+        return false;
+    }
+
+    if (b.x < 0 || b.x > grid.length || b.y < 0 || b.y > grid[0].length) {
+        return false;
+    }
+
+    if (a.x === b.x && Math.abs(a.y - b.y) < 2) {
+        return true;
+    }
+
+    if (a.y === b.y && Math.abs(a.x - b.x) < 2) {
+        return true;
+    }
+    return false;
+}
